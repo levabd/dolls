e0af8042006280488eab10538d2529007fc6a37b
Vassiliy Makaganchuk <makaganchukvv@mail.ru>
2017-09-26 13:42:26 +0600

    Scenes: ExamManager_scene, Inventory; Item

---
+++ Assets/Resources/Scripts/ControlActivateMethod.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlActivateMethod: MonoBehaviour {
+
+    public GameObject ItemPanel;
+    public Text StatusBarText;
+    
+
+
+    public void Start()
+    {
+        ItemPanel.SetActive(false);
+    }
+
+    public void ControlItems(string ActivateNewMethod)
+    {
+       switch (ActivateNewMethod)
+        {
+            case "Syringe":                
+                Debug.Log("Панель шприца");              
+                break;
+            case "Catheter":
+                //statusItem = "Катетер";
+                Debug.Log("Панель катетера");
+                break;
+        }
+    }
+
+   
+
+    public void ItemPanelActive (GameObject obj)    {
+        
+        obj.SetActive(true);
+    }
+    public void ChoseNeedle(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+    public void OpenCatheterPanel(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+    public void CloseCatheterPanel(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+}
---
+++ Assets/Resources/Scripts/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    [Header("Иконка предмета")]
+    public string sprite;
+    [Header("Физический объект")]
+    public string prefab;
+    [Header("Действие кнопки")]
+    public string action;    
+}
