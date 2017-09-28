fe2a619897d6931721b1a60c2b78f8305860cbdf
Vassiliy Makaganchuk <makaganchukvv@mail.ru>
2017-09-28 08:41:56 +0600

    Scene: Inventory Scripts: Inventory Manage Prefabs: Test ToolItems Icons: Test ToolItems

---
+++ Assets/Resources/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+    public List<ToolItem> items = new List<ToolItem>();
+    public InventoryDisplay inventoryDisplayPrefab;
+    public Dictionary<string, string> InventoryTools = new Exam1().InventoryTool;
+    // Use this for initialization
+    void Start () {
+
+        AddItemsFromExamToList();
+        InventoryDisplay inventory = (InventoryDisplay)Instantiate(inventoryDisplayPrefab);
+        inventory.Prime(items);
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void AddItemsFromExamToList() {
+        foreach (KeyValuePair<string, string> examTool in InventoryTools)
+        {// Debug.Log("Key = {0}, Value = {1}"+ examTool.Key + examTool.Value);
+
+            Debug.Log("Prefabs/Tools/" + examTool.Key);
+            GameObject tool = (GameObject)Instantiate(Resources.Load("Prefabs/Tools/" + examTool.Key));
+            tool.name = string.Format(examTool.Key);
+            Debug.Log(tool.name);
+            ToolItem it = tool.GetComponent<ToolItem>();
+            items.Add(it);
+        }
+    }
+
+}
---
+++ Assets/Resources/Scripts/Inventory/InventoryItemDisplay.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItemDisplay : MonoBehaviour {
+    public Text textName;
+    public Image sprite;
+
+    public delegate void InventoryItemDisplayDelegate(ToolItem item);
+    public static event InventoryItemDisplayDelegate onClick;
+
+    public ToolItem item;
+	// Use this for initialization
+	void Start () {
+        if (item != null) Prime(item);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Prime(ToolItem item)
+    {
+        this.item = item;
+        if (textName != null)
+        {
+            textName.text = item.title;
+        }
+        if (sprite != null)
+        {
+            sprite.sprite = item.sprite;
+        }
+    }
+
+    public void Click()
+    {
+        Debug.Log("Clicked" + item.title);
+        if (onClick != null)
+        {
+            onClick.Invoke(item);
+        }
+        else
+        {
+            Debug.Log("Nobody was listening to" + item.title);
+        }
+    }
+}
