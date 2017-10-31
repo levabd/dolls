eeb21ca7867984bee8e973e412c6f8988ef36201
ruslan <rlevitskiywork@gmail.com>
2017-10-31 11:43:16 +0200

    fix Exam8

--- Assets/Resources/Scripts/Inventory/tool_actions_menu/ToolItemActionResponder.cs
+++ Assets/Resources/Scripts/Inventory/tool_actions_menu/ToolItemActionResponder.cs
@@ -72,7 +72,7 @@ public class ToolItemActionResponder : MonoBehaviour {
         {
             logActionText = logActionText + "/" + errorMessage;
         }
-        if (CurrentTool.Instance.Tool.StateParams["entry_angle"] != "")
+        if (CurrentTool.Instance.Tool.StateParams.ContainsKey("entry_angle") && CurrentTool.Instance.Tool.StateParams["entry_angle"] != "")
         {
             logActionText = "Угол наклона шприца = " + CurrentTool.Instance.Tool.StateParams["entry_angle"];
         }
