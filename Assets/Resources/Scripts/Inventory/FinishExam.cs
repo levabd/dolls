eb990d51ab04dca89184cb0476399bef929414a8
Vassiliy Makaganchuk <makaganchukvv@mail.ru>
2017-11-17 19:04:22 +0600

    Log update fix

--- Assets/Resources/Scripts/Inventory/FinishExam.cs
+++ Assets/Resources/Scripts/Inventory/FinishExam.cs
@@ -32,6 +32,6 @@ public class FinishExam : MonoBehaviour {
         {
             examControl.EndExam(Finished, "В ходе экзамена были допущены ошибки");
         }
-                
+        CurrentExam.Instance.Exam = null;
     }
 }
