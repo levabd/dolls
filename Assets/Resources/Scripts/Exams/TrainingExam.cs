eee6e7482dd41f62058ad4df1d7dd085e26e7eb1
Vassiliy Makaganchuk <makaganchukvv@mail.ru>
2017-11-28 17:29:39 +0600

    fix blood pressure exam 1

--- Assets/Resources/Scripts/Exams/TrainingExam.cs
+++ Assets/Resources/Scripts/Exams/TrainingExam.cs
@@ -12,12 +12,15 @@ class TrainingExam : BaseExam
     public override TupleList<string, string> CorrectSteps => new TupleList<string, string>
     {
         { "wear_examination_gloves",        "Одягти оглядові рукавички" },
-        { "wear_examination_gloves",        "Одягти оглядові рукавички" },
-        { "wear_examination_gloves",        "Одягти оглядові рукавички" },
-        { "wear_examination_gloves",        "Одягти оглядові рукавички" },
-        { "wear_examination_gloves",        "Одягти оглядові рукавички" },
-        { "wear_examination_gloves",        "Одягти оглядові рукавички" },
-        { "wear_examination_gloves",        "Одягти оглядові рукавички" }
+        { "spirit_balls",                   "Промокнути марлеві кульки 70% розчином спирту" },
+        { "tweezers_spirit_balls",          "Взяти змочені марлеві кульки" },
+        { "spirit_disinfection",            "Дезінфекція спиртом. Протерти зверху вниз" },
+        { "wear_sterile_gloves",            "Змінити рукавички на стерильні" },
+        { "anesthesia_needle",              "Взяти голку для анестезії шкіри" },
+        { "anesthesia",                     "Зробити місцеву анестезію" },
+        { "puncture_needle",                "Взяти голку для пункції вени" },
+        { "puncture_novocaine",             "Наповнити 0,25% новокаїну на половину" },
+        { "disconnect_syringe",             "Від'єднати шприц від голки" }
 
     };
 
@@ -28,6 +31,10 @@ class TrainingExam : BaseExam
         { "syringe",                        "Шприц без голки" },
         { "gauze_balls",                    "Стерильні марлеві кульки" },
         { "tweezers",                       "Пінцет без нічого" },
+        { "hand",                           "Рука для додаткових дій" },
+        { "standart_catheter_conductor",    "Стандартний гнучкий провідник до катетера" },
+        { "catheter",                       "Катетер з канюлею і заглушкою" },
+        { "venflon",                        "Катетер Venflon"},
         { "patch",                          "Пластир" }
     };
 
@@ -50,20 +57,22 @@ class TrainingExam : BaseExam
                     { "piston_pulling",         "Потягування поршня на себе" },
                     { "null",                   "---" },
                     { "anesthesia_needle",      "Взяти голку для анестезії шкіри і наповнити шприц анестетиком" },
-                    { "wire_needle",            "Взяти голку для провідникової анестезії та наповнити шприц анестетиком" },
-                    { "a45_d4_punction_needle", "Взяти голку для пункції вени довжиною не менше 4 см з внутрішнім просвітом каналу 1,0-1,4 мм і зрізом вістря голки під кутом 40-45°" },
-                    { "filling_nacl",           "Наповнити 10-20 мл фізрозчином" },
+                    { "g22G_needle",            "Взяти голку для спинномозкової анестезії 22G і наповнити шприц анестетиком" },
+                    { "a45_d10_punction_needle",  "Взяти голку для пункції вени довжиною 10 см з внутрішнім просвітом каналу 1,7 мм і зрізом вістря голки під кутом 45°" },
+                    { "null",                   "---" },
+                    { "filling_novocaine_full", "Наповнити 0,25% новокаїну повністю" },
+                    { "filling_novocaine_half", "Наповнити 0,25% новокаїну наполовину" },
+                    { "filling_nacl_half",      "Наповнити 0,9% розчином натрію хлориду наполовину"}
                 };
             case "gauze_balls":
                 return new TupleList<string, string>
-                {                    
+                {
                     { "clear",       "Взяти нову стерильну кульку" },
+                    { "spirit_p70",  "Промокнути в 70% розчині спирту" },
+                    { "spirit_p60",  "Промокнути в 60% розчині спирту" },
                     { "spirit_p80",  "Промокнути в 80% розчині спирту" },
                     { "iodine_p1",   "Промокнути в 1% розчині йодоната" },
-                    { "null",        "---" },
-                    { "throw_balls", "Викинути кульки в смітник" },
-                    { "get_balls",   "Прикласти кульку" }, // "attach_balls"
-                    { "get_top_down","Протерти зверху вниз" }, // "top_down"
+                    { "iodine_p3",   "Промокнути в 3% розчині йодоната" }
                 };
             case "tweezers":
                 return new TupleList<string, string>
@@ -73,6 +82,38 @@ class TrainingExam : BaseExam
                     { "null",           "---" },
                     { "top_down",       "Протерти зверху вниз" },
                     { "right_left",     "Протерти справа наліво" }
+                };
+            case "hand":
+                return new TupleList<string, string>
+                {
+                    { "get_palpation",        "Пальпація" },// "palpation"
+                    { "get_stretch_the_skin", "Натягнути шкіру і зафіксувати вену" }, // "stretch_the_skin"
+                    { "get_clamp",            "Затиснути вену" }, //clamp
+                    { "clamp_out",            "Відпустити вену"},
+                };
+            case "standart_catheter_conductor":
+                return new TupleList<string, string>
+                {
+                    { "push", "Вставити провідник" },
+                    { "pull", "Видалити провідник" }
+                };
+            case "catheter":
+                return new TupleList<string, string>
+                {
+                    { "push",                           "Вставити катетер по провіднику" },
+                    { "remove",                         "Видалити катетер" },
+                    { "liquid_transfusion_connection",  "З'єднати з системою переливання рідин" },
+                    { "rotation_insertion",             "Поглибити обертальними рухами" },
+                    { "direct_insertion",               "Поглибити прямими рухами" }
+                };
+            case "venflon":
+                return new TupleList<string, string>
+                {
+                    { "get",                            "Взяти" },
+                    { "remove",                         "Видалити катетер" },
+                    { "liquid_transfusion_connection",  "З'єднати з системою переливання рідин" },
+                    { "remove_mandren",                 "Витягнути мадрен" },
+                    { "pull_mandren",                   "Потягнути мадрен" }
                 };
             case "patch":
                 return new TupleList<string, string>
