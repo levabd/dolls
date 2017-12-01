f832c059aa4b23ae9a0844a8322e6b1a3737a926
ruslan <rlevitskiywork@gmail.com>
2017-12-01 17:15:09 +0200

    new loader

--- Assets/Resources/Scripts/ExamSelectMenu/ChoiceExamController.cs
+++ Assets/Resources/Scripts/ExamSelectMenu/ChoiceExamController.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChoiceExamController : MonoBehaviour {
 	public bool action = false;
 	public BaseExam exam;
+    public AsyncOperation async;
+    public GameObject loader;
     SceneListCheck sceneListCheck;
+    private float load = 0;
+    private bool activeLoader;
     // Use this for initialization
     void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (action)
+
+        if (activeLoader)
+        {
+            StartCoroutine(CounterLoader());
+        }
+        if (action)
 		{
+            loader.SetActive(true);
+            activeLoader = true;
 			CheckAction();
 		}
 	}
@@ -27,6 +39,7 @@ public class ChoiceExamController : MonoBehaviour {
 
 	private void CheckAction()
 	{
+
         SceneListCheck sceneListCheck = new SceneListCheck();
         if (sceneListCheck.Has(exam.LoadName))
         {
@@ -34,7 +47,9 @@ public class ChoiceExamController : MonoBehaviour {
             CurrentExam.Instance.Exam = null;
             CurrentExam.Instance.Exam = exam;
 #pragma warning disable 618
-            Application.LoadLevel(exam.LoadName);
+
+            //Application.LoadLevel(exam.LoadName);
+            StartCoroutine(LoadNewScene(exam.LoadName));
 #pragma warning restore 618
         }
         else
@@ -43,4 +58,35 @@ public class ChoiceExamController : MonoBehaviour {
         }    
 		action = false;
 	}
+
+    IEnumerator LoadNewScene(string scene)
+    {
+
+        yield return new WaitForSeconds(3);
+
+        async = Application.LoadLevelAsync(scene);
+
+        async.allowSceneActivation = false;
+
+        while (!async.isDone)
+        {
+            Debug.Log(async.progress);
+            if (load == 100)
+            {
+                async.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+
+    }
+
+    IEnumerator CounterLoader()
+    {
+        yield return new WaitForSeconds(1);
+        if (load < 100)
+        {
+            load += 0.5f;
+            GameObject.Find("Main Interface/ProgressRadialPlain").transform.GetChild(1).GetChild(0).transform.gameObject.GetComponent<Text>().text = $"{System.Convert.ToInt32(load)}%";
+        }
+    }
 }
